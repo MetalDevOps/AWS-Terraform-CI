3b1738aac9bc7f572bd37bdceba6bf96ae0b579e
Egor Nats <kyo@kyo.ninja>
2022-04-17 16:04:14 -0300

    Testing a new deploy workflow

---
+++ aws_oidc/aws_oidc.tf
@@ -0,0 +1,60 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:whoamikyo/*:*",
+      ]
+    }
+  }
+}
+
+  resource "aws_iam_role" "github_actions" {
+  name               = "github-actions"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+data "aws_iam_policy_document" "github_actions" {
+  statement {
+    actions = [
+      "s3:*",
+      "ec2:*",
+      "cloudwatch:*",
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:ResourceTag/permit-github-action"
+
+      values = ["true"]
+    }
+  }
+}
+
+  resource "aws_iam_policy" "github_actions" {
+  name        = "github-actions"
+  description = "Grant Github Actions the ability to have access to S3, EC2 and Cloudwatch"
+  policy      = data.aws_iam_policy_document.github_actions.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions.arn
+}
