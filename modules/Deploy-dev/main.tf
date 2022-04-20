7acdf8210fa874a1a786da625b9ec850ae471c8c
Egor Nats <kyo@kyo.ninja>
2022-04-19 21:25:02 -0300

    - Changed master ami to CentOs
    - Added AmazonEC2FullAccess to codedeploy-ec2-role

--- modules/Deploy-dev/main.tf
+++ modules/Deploy-dev/main.tf
@@ -8,12 +8,21 @@ resource "aws_key_pair" "ninja-key" {
 }
 
 resource "aws_security_group" "ninja-sg" {
+  name        = "ninja-sg"
+  description = "Security group apenas para liberar acesso as portas 80/443/22"
   
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    self = true
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -30,7 +39,8 @@ resource "aws_security_group" "ninja-sg" {
     protocol    = "-1"
   }
 
-} 
+}
+
 
 resource "aws_instance" "ninja-worker" {
   ami           = "ami-0e472ba40eb589f49"
@@ -45,7 +55,7 @@ resource "aws_instance" "ninja-worker" {
 }
 
 resource "aws_instance" "ninja-master" {
-  ami           = "ami-0e472ba40eb589f49"
+  ami           = "ami-03ededff12e34e59e"
   instance_type = "t2.micro"
   key_name = "ninja-key"
   count = 1
@@ -53,5 +63,90 @@ resource "aws_instance" "ninja-master" {
     name = "ninja"
     type = "master"
   }
+  
+  iam_instance_profile = aws_iam_instance_profile.ec2-role_profile.name
+
   security_groups = ["${aws_security_group.ninja-sg.name}"]
+
+  user_data = data.template_file.user_data.rendered
+
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/user-data.sh")
+
+}
+
+resource "aws_iam_instance_profile" "ec2-role_profile" {
+  name =  "codedeploy-ec2-role-profile"
+  role =  aws_iam_role.codedeploy-ec2-role.name
+}
+
+resource "aws_iam_role" "codedeploy-ec2-role" {
+  name = "codedeploy-ec2-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com",
+        "Service": "codedeploy.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    Name          = "codedeploy-ec2-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-role-attach-cd" {
+  role        = aws_iam_role.codedeploy-ec2-role.name
+  policy_arn  = "arn:aws:iam::aws:policy/AWSCodeDeployFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-role-attach-s3" {
+  role        = aws_iam_role.codedeploy-ec2-role.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-role-attach-ec2" {
+  role        = aws_iam_role.codedeploy-ec2-role.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role" "codedeploy-role" {
+  name = "codedeploy-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codedeploy.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    Name          = "codedeploy-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "cd-role-attach" {
+  role        = aws_iam_role.codedeploy-role.name
+  policy_arn  = "arn:aws:iam::aws:policy/AWSCodeDeployFullAccess"
 }
